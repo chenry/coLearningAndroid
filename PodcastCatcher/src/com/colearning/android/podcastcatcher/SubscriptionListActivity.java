a518cd4be3f16ce76278201b0b8377024fba0fd2
Carlus Henry <carlushenry@gmail.com>
2013-11-08 20:40:01 -0500

    adding the onefragment

--- PodcastCatcher/src/com/colearning/android/podcastcatcher/SubscriptionListActivity.java
+++ PodcastCatcher/src/com/colearning/android/podcastcatcher/SubscriptionListActivity.java
@@ -1,28 +1,11 @@
 package com.colearning.android.podcastcatcher;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 
-public class SubscriptionListActivity extends FragmentActivity {
+public class SubscriptionListActivity extends OneFragmentFragmentActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_single_fragment);
-
-		FragmentManager fm = getSupportFragmentManager();
-		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
-		if (fragment == null) {
-			fragment = new SubscriptionListFragment();
-			//@formatter:off
-			fm
-				.beginTransaction()
-				.add(R.id.fragmentContainer, fragment)
-				.commit();
-			//@formatter:on
-		}
+	public Fragment createFragment() {
+		return new SubscriptionListFragment();
 	}
-
 }
--- PodcastCatcher/src/com/colearning/android/podcastcatcher/SubscriptionListFragment.java
+++ PodcastCatcher/src/com/colearning/android/podcastcatcher/SubscriptionListFragment.java
@@ -4,16 +4,18 @@ import java.util.List;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.colearning.android.podcastcatcher.model.Subscription;
 import com.colearning.android.podcastcatcher.model.SubscriptionList;
 
 public class SubscriptionListFragment extends ListFragment {
-
+	private static final String TAG = "SubscriptionListFragment";
 	private List<Subscription> subscriptions;
 
 	@Override
@@ -23,6 +25,14 @@ public class SubscriptionListFragment extends ListFragment {
 		subscriptions = SubscriptionList.create().getSubscriptions();
 
 		setListAdapter(new SubscriptionListAdapter(subscriptions));
+	}
+
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		Log.i(TAG, "ListView: " + l);
+		Log.i(TAG, "View: " + v);
+		Log.i(TAG, "position: " + position);
+		Log.i(TAG, "id: " + id);
 	}
 
 	private class SubscriptionListAdapter extends ArrayAdapter<Subscription> {
