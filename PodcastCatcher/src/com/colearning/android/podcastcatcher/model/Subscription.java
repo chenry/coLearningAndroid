f4e49a7a972cd995f0b06bafb3409bf2ecb85e9d
Carlus Henry <carlushenry@gmail.com>
2013-11-10 09:17:09 -0500

    adding new properties to be displayed on the new screen

--- PodcastCatcher/src/com/colearning/android/podcastcatcher/model/Subscription.java
+++ PodcastCatcher/src/com/colearning/android/podcastcatcher/model/Subscription.java
@@ -4,6 +4,11 @@ public class Subscription {
 
 	private String title;
 	private String subTitle;
+	private String author;
+	private String summary;
+	private String category;
+	private String link;
+	private String imageUrl;
 
 	public Subscription(String title, String subTitle) {
 		this.title = title;
@@ -17,4 +22,45 @@ public class Subscription {
 	public String getTitle() {
 		return title;
 	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
 }
--- PodcastCatcher/src/com/colearning/android/podcastcatcher/model/SubscriptionList.java
+++ PodcastCatcher/src/com/colearning/android/podcastcatcher/model/SubscriptionList.java
@@ -12,6 +12,13 @@ public class SubscriptionList {
 		subscriptions = new ArrayList<Subscription>();
 		for (int i = 0; i < 20; i++) {
 			Subscription subscription = new Subscription("Title #" + i, "Subtitle: " + i);
+			subscription.setAuthor("Peter Pan " + i);
+			subscription.setCategory("Tech");
+			subscription.setImageUrl("https://www.google.com/images/srpr/logo11w.png");
+			subscription.setLink("http://agiletoolkit.libsyn.com");
+			subscription
+					.setSummary(i
+							+ "The Empire Podcast is the last word on movies from the biggest movie magazine on the planet, including each week's news, reviews and interviews");
 			subscriptions.add(subscription);
 		}
 	}
