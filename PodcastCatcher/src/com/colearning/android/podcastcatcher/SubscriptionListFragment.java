4996f2a721cc80b0f64f7c7107a55b8efcb2c729
Carlus Henry <carlushenry@gmail.com>
2013-11-14 13:12:59 -0500

    commenting out all of the Feedparser logic...

--- PodcastCatcher/src/com/colearning/android/podcastcatcher/SubscriptionListFragment.java
+++ PodcastCatcher/src/com/colearning/android/podcastcatcher/SubscriptionListFragment.java
@@ -5,14 +5,12 @@ import java.util.List;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.colearning.android.podcastcatcher.feed.FeedParser;
 import com.colearning.android.podcastcatcher.model.Subscription;
 import com.colearning.android.podcastcatcher.model.SubscriptionList;
 
@@ -32,13 +30,18 @@ public class SubscriptionListFragment extends ListFragment {
 		subscriptions = SubscriptionList.create().getSubscriptions();
 		setListAdapter(new SubscriptionListAdapter(subscriptions));
 
-		FeedParser feedParser = new FeedParser();
-		String urlPath1 = "http://feeds.feedburner.com/javaposse";
-		String urlPath2 = "http://feeds.feedburner.com/AndroidCentralPodcast";
-		Subscription javaPosseSubscription = feedParser.parseSubscription(urlPath1);
-		Log.i(TAG, "Subscription: Title: " + javaPosseSubscription.getTitle() + ", sub: " + javaPosseSubscription.getSubTitle());
-		Subscription androidPodcastSubscription = feedParser.parseSubscription(urlPath2);
-		Log.i(TAG, "Subscription: Title: " + androidPodcastSubscription.getTitle() + ", sub: " + androidPodcastSubscription.getSubTitle());
+		/*
+		 * FeedParser feedParser = new FeedParser(); String urlPath1 =
+		 * "http://feeds.feedburner.com/javaposse"; String urlPath2 =
+		 * "http://feeds.feedburner.com/AndroidCentralPodcast"; Subscription
+		 * javaPosseSubscription = feedParser.parseSubscription(urlPath1);
+		 * Log.i(TAG, "Subscription: Title: " + javaPosseSubscription.getTitle()
+		 * + ", sub: " + javaPosseSubscription.getSubTitle()); Subscription
+		 * androidPodcastSubscription = feedParser.parseSubscription(urlPath2);
+		 * Log.i(TAG, "Subscription: Title: " +
+		 * androidPodcastSubscription.getTitle() + ", sub: " +
+		 * androidPodcastSubscription.getSubTitle());
+		 */
 	}
 
 	@Override
