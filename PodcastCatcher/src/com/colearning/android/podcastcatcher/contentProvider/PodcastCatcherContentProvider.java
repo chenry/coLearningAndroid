ff5fce811416368b87a0f439ddc82d67e90cdc38
Carlus Henry <carlushenry@gmail.com>
2013-11-20 12:57:01 -0500

    successfully added a contract object....starting to work on query contract provider.

---
+++ PodcastCatcher/src/com/colearning/android/podcastcatcher/contentProvider/PodcastCatcherContentProvider.java
@@ -0,0 +1,58 @@
+package com.colearning.android.podcastcatcher.contentProvider;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+
+import com.colearning.android.podcastcatcher.contract.PodcastCatcherContract;
+import com.colearning.android.podcastcatcher.manager.PodcastCatcherManager;
+
+public class PodcastCatcherContentProvider extends ContentProvider {
+
+	public static final int SUBSCRIPTIONS = 100;
+	public static final int SUBSCRIPTION_ID = 110;
+
+	private static final UriMatcher sURIMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+	static {
+		sURIMatcher.addURI(PodcastCatcherContract.AUTHORITY, "subscriptions", SUBSCRIPTIONS);
+		sURIMatcher.addURI(PodcastCatcherContract.AUTHORITY, "subscription/#", SUBSCRIPTION_ID);
+	}
+
+	@Override
+	public int delete(Uri arg0, String arg1, String[] arg2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getType(Uri uri) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Uri insert(Uri uri, ContentValues values) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean onCreate() {
+		podcastCatcherManager = PodcastCatcherManager.create(getContext());
+		return true;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+		return null;
+	}
+
+	@Override
+	public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- PodcastCatcher/src/com/colearning/android/podcastcatcher/contract/PodcastCatcherContract.java
+++ PodcastCatcher/src/com/colearning/android/podcastcatcher/contract/PodcastCatcherContract.java
@@ -1,8 +1,17 @@
 package com.colearning.android.podcastcatcher.contract;
 
+import android.content.ContentResolver;
+import android.net.Uri;
+
 public final class PodcastCatcherContract {
-	
+	public static final String AUTHORITY = "com.colearning.android.podcstcatcher";
+	public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
 	public static final class Subscription {
+		public static final Uri CONTENT_URI = Uri.withAppendedPath(PodcastCatcherContract.CONTENT_URI, "subscriptions");
+		public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + AUTHORITY + ".subscriptions";
+		public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + AUTHORITY + ".subscriptions";
+
 		public static final class Columns {
 			public static final String _ID = "_id";
 			public static final String FEED_URL = "feed_url";
@@ -15,9 +24,16 @@ public final class PodcastCatcherContract {
 			public static final String LAST_SYNC_TIME = "last_sync_time";
 			public static final String LAST_PUB_DATE = "last_pub_date";
 		}
+
+		public static final String[] PROJECTION_ALL = { Columns._ID, Columns.FEED_URL, Columns.TITLE, Columns.SUBTITLE, Columns.AUTHOR, Columns.SUMMARY,
+				Columns.CATEGORY, Columns.IMAGE_URL, Columns.LAST_SYNC_TIME, Columns.LAST_PUB_DATE };
 	}
-	
+
 	public static final class SubscriptionItem {
+		public static final Uri CONTENT_URI = Uri.withAppendedPath(PodcastCatcherContract.CONTENT_URI, "subscriptionItems");
+		public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + AUTHORITY + ".subscriptionItems";
+		public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + AUTHORITY + ".subscriptionItems";
+
 		public static final class Columns {
 			public static final String _ID = "_id";
 			public static final String SUBSCRIPTION_ID = "subscription_id";
@@ -28,9 +44,11 @@ public final class PodcastCatcherContract {
 			public static final String ITEM_DESC = "item_desc";
 			public static final String MEDIA_URL = "media_url";
 			public static final String FILE_LOCATION = "file_location";
-			
+
 		}
-		
+
+		public static final String[] PROJECTION_ALL = { Columns._ID, Columns.SUBSCRIPTION_ID, Columns.TITLE, Columns.GUID_ID, Columns.LINK_URL,
+				Columns.THUMBNAIL_URL, Columns.ITEM_DESC, Columns.MEDIA_URL, Columns.FILE_LOCATION };
 	}
-	
+
 }
